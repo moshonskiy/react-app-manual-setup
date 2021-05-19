4b3eec7c282dee9fe44cabec783d70ac25b45a0e
Kirill Moshonskiy <furiousbenjamin@gmail.com>
2021-05-19 22:25:57 +0300

    React app manual setup

---
+++ src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Manual setup of react app (no create-react-app)</h1>
+        </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modules: [path.join(__dirname, 'src'), 'node_modules'],
+        alias: {
+            react: path.join(__dirname, 'node_modules', 'react'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        })
+    ]
+};
